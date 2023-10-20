45b8ceed51e6f77b9b4d883e96d6d2de1db00f00
Gaute <mrboobai@proton.me>
2023-10-20 09:50:25 +0200

    js

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ let generateBtn = document.getElementById(generatebtn);
 let groupNames = document.getElementById(groupnames);
 let groupOutput = document.getElementById(groupoutput);
 
-let groupNamesCont = groupoutput.textContent;
+let groupNamesContent = groupoutput.textContent;
+
+print(groupoutput.textContent);
+console.log(groupoutput.textContent);
 
 function genGroup() {
-    print(groupNamesCont)
-    console.log(groupNamesCont)
+    print(groupNamesContent)
+    console.log(groupNamesContent)
 }
+genGroup();
