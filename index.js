e076bcda6fb1fac4e8347ecb6a117f6e242847a2
Andolo-64 <andreasolavhogestol@gmail.com>
2023-10-20 14:22:07 +0200

    slidnum

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function genBtn() {
     const tests = document.getElementById("output");
-    var valueInVar = '';
+    var valueInVar = ''; 
     valueInVar = document.getElementById("name").value;
-
-    tests.innerHTML = (valueInVar)
+    tests.innerHTML = (valueInVar)  
+}
+function Slid() {
+    const sliderTall = document.getElementById("sliderTall");
+    SliderTall = document.getElementById("GrupeSlider").value;
+    var SliderTall = '';
+    sliderTall.innerHTML = (SliderTall)
 }
