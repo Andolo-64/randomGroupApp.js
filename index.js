2359c7dc0b5c78b3e4311affc69e22ce7f0b0f10
Gaute <mrboobai@proton.me>
2023-10-20 08:57:24 +0200

    update

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ let generateBtn = document.getElementById(generateBtn);
 let groupNames = document.getElementById(groupNames);
 let groupOutput = document.getElementById(groupOutput);
 
+if () {
+    
+}
